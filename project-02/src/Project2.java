c71312d4611e8d3c1a86cc49b8e86f0c18a77614
Nicole <nzolnier@gmail.com>
2022-03-25 12:29:52 -0400

    add: add and print faculty

--- project-02/src/Project2.java
+++ project-02/src/Project2.java
@@ -21,49 +21,12 @@ public class Project2 {
         System.out.println(s);
         System.out.println(t);
 
-        displayMenu();
-        String ans = choiceInput();
-
-        while (!ans.equals("7")) {
-            if (ans.equals("1")) {
-                System.out.println("add new faculty");
-                break;
-            } else if (ans.equals("2")) {
-                System.out.println("enroll student");
-                break;
-            } else if (ans.equals("3")) {
-                System.out.println("print schedule faculty");
-                break;
-            } else if (ans.equals("4")) {
-                System.out.println("print schedule ta");
-                break;
-            } else if (ans.equals("5")) {
-                System.out.println("print schedule student");
-                break;
-            } else if (ans.equals("6")) {
-                deleteLecture(ucfLectures);
-                break;
-            } else {
-                displayMenu();
-                ans = choiceInput();
-                break;
-            }
-        }
+        loopMenuChoices(ucfLectures, ucfPeople);
 
         displayGoodbye();
     }
 
-    public static void displayMenu() {
-        System.out.println("___________Choose one of the options____________________");
-        System.out.println("1. Add a new faculty to the schedule.");
-        System.out.println("2. Enroll a student to a lecture.");
-        System.out.println("3. Print the Schedule of a faculty.");
-        System.out.println("4. Print the schedule of an TA.");
-        System.out.println("5. Print the schedule of a student.");
-        System.out.println("6. Delete a scheduled lecture.");
-        System.out.println("7. Exit Program.");
-    }
-
+    // input methods
     public static String choiceInput() {
         System.out.println("\tEnter your selection: ");
         return new Scanner(System.in).nextLine();
@@ -78,11 +41,7 @@ public class Project2 {
         System.out.println(prompt);
         return new Scanner(System.in).nextLine();
     }
-
-    public static void displayGoodbye() {
-        System.out.println("Goodbye! ");
-    }
-
+    //-------------
     public static ArrayList<Lecture> setup() throws FileNotFoundException {
         ArrayList<Lecture> lecs = new ArrayList<Lecture>();
         String buffer, prefix, modality, location, description;
@@ -131,27 +90,169 @@ public class Project2 {
         return lecs;
     }
 
-    public static void deleteLecture(ArrayList<Lecture> lecs) {
-        int crn = intInput("Enter crn of the lecture to delete:");
-        boolean inLecs = false;
-        Lecture lec = new Lecture();
+    public static void displayMenu() {
+        System.out.println("___________Choose one of the options____________________");
+        System.out.println("0. Add a new TA to the schedule");
+        System.out.println("1. Add a new faculty to the schedule.");
+        System.out.println("2. Enroll a student to a lecture.");
+        System.out.println("3. Print the Schedule of a faculty.");
+        System.out.println("4. Print the schedule of an TA.");
+        System.out.println("5. Print the schedule of a student.");
+        System.out.println("6. Delete a scheduled lecture.");
+        System.out.println("7. Exit Program.");
+    }
 
+    public static void loopMenuChoices(ArrayList<Lecture> lecs, ArrayList<Person> people) {
+        displayMenu();
+        String ans = choiceInput();
+
+        while (!ans.equals("7")) {
+            if (ans.equals("0")) {
+                System.out.println("add ta");
+                break;
+            } else if (ans.equals("1")) {
+                addFaculty(lecs, people);
+                break;
+            } else if (ans.equals("2")) {
+                System.out.println("enroll student");
+                break;
+            } else if (ans.equals("3")) {
+                printFaculty(people);
+                break;
+            } else if (ans.equals("4")) {
+                System.out.println("print schedule ta");
+                break;
+            } else if (ans.equals("5")) {
+                System.out.println("print schedule student");
+                break;
+            } else if (ans.equals("6")) {
+                System.out.println("delete lecture from faculty/student/ta");
+                break;
+            } else {
+                displayMenu();
+                ans = choiceInput();
+                break;
+            }
+        }
+    }
+
+    public static void addFaculty(ArrayList<Lecture> lecs, ArrayList<Person> people) {
+        String name = stringInput("Enter name: ");
+        int id = intInput("Enter UCF id: ");
+        while(checkValidId(id, people)) {
+            id = intInput("Sorry, that UCF id is already in use. Enter a new UCF id: ");
+        }
+        String rank = stringInput("Enter rank: ");
+        String officeLocation = stringInput("Enter office location: ");
+        int numLecs = intInput("Enter how many lectures");
+        ArrayList<Lecture> lecturesTaught = new ArrayList<Lecture>();
+
+        int crn;
+        for(int i = 0; i < numLecs; i++) {
+            crn = intInput("Enter the crn of the" + i + "° lecture to assign to this faculty");
+            if(lectureExists(crn, lecs)) {
+                if(findLecture(crn, lecs).lab) {
+                    for(int j = 0; j < findLecture(crn, lecs).labs.size(); j++) {
+                        int taId = intInput("Enter the UCF id of the TA");
+
+                        if(taExists(taId, people)) {
+                            findTA(taId, people).addLabSupervised(findLecture(crn, lecs).labs.get(j));
+                        }
+                    }
+                } 
+                
+                lecturesTaught.add(findLecture(crn, lecs));   
+            }
+        }
+
+        System.out.println("New faculty added!");
+        people.add(new Faculty(name, id, rank, officeLocation, lecturesTaught));
+
+        loopMenuChoices(lecs, people);
+    }
+
+
+    public static void printFaculty(ArrayList<Person> people) {
+        int id = intInput("Enter UCF id: ");
+
+        if(facultyExists(id, people)) {
+            System.out.println(findFaculty(id, people));
+        } else {
+            System.out.println("Faculty not found");
+            printFaculty(people);
+        }
+    }
+
+    public static boolean taExists(int id, ArrayList<Person> people) {
+        for (Person p : people) {
+            if (p.id == id && p instanceof TA) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean facultyExists(int id, ArrayList<Person> people) {
+        for (Person p : people) {
+            if (p.id == id && p instanceof Faculty) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static TA findTA(int id, ArrayList<Person> people) {
+        TA t = new TA();
+        for (Person p : people) {
+            if (p.id == id && p instanceof TA) {
+                t= (TA) p;
+            }
+        }
+        return t;
+    }
+
+
+    public static Faculty findFaculty(int id, ArrayList<Person> people) {
+        Faculty f = new Faculty();
+        for (Person p : people) {
+            if (p.id == id && p instanceof Faculty) {
+                f = (Faculty) p;
+            }
+        }
+        return f;
+    }
+
+    public static boolean checkValidId(int id, ArrayList<Person> people) {
+        for (Person p : people) {
+            if (p.id == id) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean lectureExists(int crn, ArrayList<Lecture> lecs) {
         for (Lecture l : lecs) {
             if (l.courseNumber == crn) {
-                inLecs = true;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static Lecture findLecture(int crn, ArrayList<Lecture> lecs) {
+        Lecture lec = new Lecture();
+        for (Lecture l : lecs) {
+            if (l.courseNumber == crn) {
                 lec = l;
                 break;
             }
         }
+        return lec;
+    }
 
-        if (inLecs) {
-            int index = lecs.indexOf(lec);
-            lecs.remove(index);
-            System.out.println("Ok, " + crn + " has been deleted.");
-
-        } else {
-            System.out.println("Sorry, lecture not found");
-        }
+    public static void displayGoodbye() {
+        System.out.println("Goodbye! ");
     }
 }
 
@@ -208,6 +309,10 @@ abstract class Person {
     public String name;
     public int id;
 
+
+    public Person() {
+    }
+
     public Person(String name, int id) {
         this.name = name;
         this.id = id;
@@ -222,6 +327,9 @@ class Faculty extends Person {
     public String rank;
     public String officeLocation;
     public ArrayList<Lecture> lecturesTaught;
+
+    public Faculty() {
+    }
 
     public Faculty(String name, int id, String rank, String officeLocation, ArrayList<Lecture> lecturesTaught) {
         super(name, id);
@@ -240,6 +348,10 @@ class Student extends Person {
     public boolean graduate;
     public ArrayList<Lecture> lecturesTaken;
 
+
+    public Student() {
+    }
+
     public Student(String name, int id, boolean graduate, ArrayList<Lecture> lecturesTaken) {
         super(name, id);
         this.graduate = graduate;
@@ -256,6 +368,13 @@ class TA extends Student {
     public ArrayList<Lab> labsSupervised;
     public Faculty advisor;
     public String expectedDegree;
+
+    public void addLabSupervised(Lab l) {
+        labsSupervised.add(l);
+    }
+
+    public TA() {
+    }
 
     public TA(String name, int id, boolean graduate, ArrayList<Lecture> lecturesTaken, ArrayList<Lab> labsSupervised,
             Faculty advisor, String expectedDegree) {
