51ec2b27af4894d5435e3dc90578485793a9e188
Nicole <nzolnier@gmail.com>
2022-03-28 12:28:45 -0400

    add: almost everything

--- project-02/src/Project2.java
+++ project-02/src/Project2.java
@@ -1,11 +1,12 @@
 
 /*
-Nicole Zolnier and Trae Roy
+Nicole Zolnier
 Project 2
 */
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Project2 {
@@ -15,8 +16,10 @@ public class Project2 {
 
         // testing out constructors and toString
         Faculty f = new Faculty("John", 5555, "Professor", "CB2", ucfLectures);
-        Student s = new Student("Sam", 2222, true, ucfLectures);
-        TA t = new TA("Anna", 3333, false, ucfLectures, ucfLectures.get(3).labs, f, "phd");
+        Student s = new Student("Sam", 2222, true, ucfLectures, ucfLectures.get(3).labs);
+
+        TA t = new TA("Anna", 3333, false, ucfLectures, ucfLectures.get(3).labs, ucfLectures.get(3).labs, "John",
+                "phd");
         System.out.println(f);
         System.out.println(s);
         System.out.println(t);
@@ -41,7 +44,8 @@ public class Project2 {
         System.out.println(prompt);
         return new Scanner(System.in).nextLine();
     }
-    //-------------
+
+    // -------------
     public static ArrayList<Lecture> setup() throws FileNotFoundException {
         ArrayList<Lecture> lecs = new ArrayList<Lecture>();
         String buffer, prefix, modality, location, description;
@@ -70,12 +74,12 @@ public class Project2 {
                 courseNumber = Integer.parseInt(token[0]);
                 prefix = token[1];
                 description = token[2];
-                graduate = (token[3].toLowerCase().equals("graduate"));
+                graduate = (token[3].equalsIgnoreCase("graduate"));
                 modality = token[4];
                 // if the modality is online, it ends on token[4]
-                if (modality.toLowerCase().equals("f2f")) {
+                if (modality.equalsIgnoreCase("f2f")) {
                     location = token[5];
-                    lab = (token[6].toLowerCase().equals("yes"));
+                    lab = (token[6].equalsIgnoreCase("yes"));
                 } else {
                     location = "";
                     lab = false;
@@ -92,7 +96,6 @@ public class Project2 {
 
     public static void displayMenu() {
         System.out.println("___________Choose one of the options____________________");
-        System.out.println("0. Add a new TA to the schedule");
         System.out.println("1. Add a new faculty to the schedule.");
         System.out.println("2. Enroll a student to a lecture.");
         System.out.println("3. Print the Schedule of a faculty.");
@@ -103,43 +106,33 @@ public class Project2 {
     }
 
     public static void loopMenuChoices(ArrayList<Lecture> lecs, ArrayList<Person> people) {
-        displayMenu();
-        String ans = choiceInput();
-
-        while (!ans.equals("7")) {
-            if (ans.equals("0")) {
-                System.out.println("add ta");
-                break;
-            } else if (ans.equals("1")) {
+        String ans;
+        do {
+            displayMenu();
+            ans = choiceInput();
+            if (ans.equals("1")) {
                 addFaculty(lecs, people);
-                break;
             } else if (ans.equals("2")) {
-                System.out.println("enroll student");
-                break;
+                addStudent(lecs, people);
             } else if (ans.equals("3")) {
                 printFaculty(people);
-                break;
             } else if (ans.equals("4")) {
-                System.out.println("print schedule ta");
-                break;
+                printTA(people);
             } else if (ans.equals("5")) {
-                System.out.println("print schedule student");
-                break;
+                printStudent(people);
             } else if (ans.equals("6")) {
-                System.out.println("delete lecture from faculty/student/ta");
-                break;
-            } else {
-                displayMenu();
-                ans = choiceInput();
+                deleteLecture(lecs, people);
+            } else if (ans.equals("7")) {
                 break;
             }
-        }
+
+        } while (!ans.equals("7"));
     }
 
     public static void addFaculty(ArrayList<Lecture> lecs, ArrayList<Person> people) {
         String name = stringInput("Enter name: ");
         int id = intInput("Enter UCF id: ");
-        while(checkValidId(id, people)) {
+        while (checkValidId(id, people)) {
             id = intInput("Sorry, that UCF id is already in use. Enter a new UCF id: ");
         }
         String rank = stringInput("Enter rank: ");
@@ -148,44 +141,190 @@ public class Project2 {
         ArrayList<Lecture> lecturesTaught = new ArrayList<Lecture>();
 
         int crn;
-        for(int i = 0; i < numLecs; i++) {
-            crn = intInput("Enter the crn of the" + i + "° lecture to assign to this faculty");
-            if(lectureExists(crn, lecs)) {
-                if(findLecture(crn, lecs).lab) {
-                    for(int j = 0; j < findLecture(crn, lecs).labs.size(); j++) {
+        for (int i = 0; i < numLecs; i++) {
+            crn = intInput("Enter the crn of the " + (i + 1) + "° lecture to assign to this faculty");
+            if (lectureExists(crn, lecs)) {
+                if (findLecture(crn, lecs).lab) {
+                    for (int j = 0; j < findLecture(crn, lecs).labs.size(); j++) {
                         int taId = intInput("Enter the UCF id of the TA");
 
-                        if(taExists(taId, people)) {
+                        if (taExists(taId, people)) {
+                            // TODO print added
                             findTA(taId, people).addLabSupervised(findLecture(crn, lecs).labs.get(j));
+                        } else {
+                            String taName = stringInput("Enter name of the TA");
+                            boolean graduate = true;
+                            String advisorName = stringInput("Enter TA's supervisor's name:");
+                            String expectedDegree = stringInput("Enter TA's degree seeking");
+
+                            people.add(new TA(taName, taId, graduate, advisorName, expectedDegree));
+
                         }
                     }
-                } 
-                
-                lecturesTaught.add(findLecture(crn, lecs));   
+                }
+
+                // TODO print lecture prefix/name
+                lecturesTaught.add(findLecture(crn, lecs));
             }
         }
 
-        System.out.println("New faculty added!");
         people.add(new Faculty(name, id, rank, officeLocation, lecturesTaught));
 
-        loopMenuChoices(lecs, people);
+        System.out.println("New faculty added!");
+
     }
 
+    public static void addStudent(ArrayList<Lecture> lecs, ArrayList<Person> people) {
+        int id = intInput("Enter UCF id: ");
+        if (studentExists(id, people)) {
+            int numLecs = intInput("Enter how many lectures");
+            int crn;
+
+            for (int i = 0; i < numLecs; i++) {
+                crn = intInput("Enter the crn of the " + (i + 1) + "° lecture to enroll student");
+                if (lectureExists(crn, lecs)) {
+                    if (findLecture(crn, lecs).graduate && !findStudent(id, people).graduate) {
+                        System.out.println("This is a graduate level class");
+                        continue;
+                    }
+                    if (findLecture(crn, lecs).lab) {
+                        Random myRand = new Random();
+
+                        int randomLab = myRand.nextInt(findLecture(crn, lecs).labs.size() + 1);
+
+                        // TODO print lecture prefix/name
+                        findStudent(id, people).labsTaken.add(findLecture(crn, lecs).labs.get(randomLab));
+                    }
+                    findStudent(id, people).lecturesTaken.add(findLecture(crn, lecs));
+                }
+            }
+
+            System.out.println("Student enrolled!");
+        } else if (taExists(id, people)) {
+
+            int numLecs = intInput("Enter how many lectures");
+            int crn;
+
+            for (int i = 0; i < numLecs; i++) {
+                crn = intInput("Enter the crn of the " + (i + 1) + "° lecture to enroll student");
+                if (lectureExists(crn, lecs)) {
+                    if (findLecture(crn, lecs).graduate && !findTA(id, people).graduate) {
+                        System.out.println("This is a graduate level class");
+                        continue;
+                    }
+                    if (findLecture(crn, lecs).lab) {
+                        Random myRand = new Random();
+
+                        int randomLab = myRand.nextInt(findLecture(crn, lecs).labs.size() + 1);
+
+                        // TODO print lecture prefix/name
+                        findTA(id, people).labsTaken.add(findLecture(crn, lecs).labs.get(randomLab));
+                    }
+                    findTA(id, people).lecturesTaken.add(findLecture(crn, lecs));
+                }
+            }
+
+            System.out.println("Student enrolled!");
+
+        } else {
+            String name = stringInput("Enter name:");
+            String grad = stringInput("Are you a graduate student?");
+            boolean graduate = (grad.equalsIgnoreCase("yes"));
+
+            int numLecs = intInput("Enter how many lectures");
+            ArrayList<Lecture> lecturesTaken = new ArrayList<Lecture>();
+            ArrayList<Lab> labsTaken = new ArrayList<Lab>();
+
+            int crn;
+            for (int i = 0; i < numLecs; i++) {
+                crn = intInput("Enter the crn of the " + (i + 1) + "° lecture to enroll student");
+                if (lectureExists(crn, lecs)) {
+                    if (findLecture(crn, lecs).graduate && !graduate) {
+                        System.out.println("This is a graduate level class");
+                        continue;
+                    }
+                    if (findLecture(crn, lecs).lab) {
+                        Random myRand = new Random();
+
+                        int randomLab = myRand.nextInt(findLecture(crn, lecs).labs.size() + 1);
+
+                        labsTaken.add(findLecture(crn, lecs).labs.get(randomLab));
+                    }
+                    // TODO print lecture prefix/name
+                    lecturesTaken.add(findLecture(crn, lecs));
+                }
+            }
+
+            people.add(new Student(name, id, graduate, lecturesTaken, labsTaken));
+
+            System.out.println("New student enrolled!");
+        }
+
+    }
 
     public static void printFaculty(ArrayList<Person> people) {
         int id = intInput("Enter UCF id: ");
 
-        if(facultyExists(id, people)) {
+        if (facultyExists(id, people)) {
             System.out.println(findFaculty(id, people));
-        } else {
-            System.out.println("Faculty not found");
-            printFaculty(people);
         }
+
+    }
+
+    public static void printStudent(ArrayList<Person> people) {
+        int id = intInput("Enter UCF id: ");
+
+        if (studentExists(id, people)) {
+            System.out.println(findStudent(id, people));
+        }
+
+    }
+
+    public static void printTA(ArrayList<Person> people) {
+        int id = intInput("Enter UCF id: ");
+
+        if (taExists(id, people)) {
+            System.out.println(findTA(id, people));
+        }
+
+    }
+
+    public static void deleteLecture(ArrayList<Lecture> lecs, ArrayList<Person> people) {
+        int crn = intInput("Enter crn of the lecture to delete:");
+        if (lectureExists(crn, lecs)) {
+            lecs.remove(findLecture(crn, lecs));
+
+            for (Person p : people) {
+                if (p instanceof Faculty && ((Faculty) p).lecturesTaught.contains(findLecture(crn, lecs))) {
+                    ((Faculty) p).lecturesTaught.remove(findLecture(crn, lecs));
+                }
+
+                if (p instanceof Student && ((Student) p).lecturesTaken.contains(findLecture(crn, lecs))) {
+                    ((Student) p).lecturesTaken.remove(findLecture(crn, lecs));
+                }
+
+                if (p instanceof TA && ((TA) p).lecturesTaken.contains(findLecture(crn, lecs))) {
+                    ((TA) p).lecturesTaken.remove(findLecture(crn, lecs));
+                }
+
+            }
+
+            // TODO delete lab
+        }
+    }
+
+    public static boolean studentExists(int id, ArrayList<Person> people) {
+        for (Person p : people) {
+            if (p.id == id && p instanceof Student) {
+                return true;
+            }
+        }
+        return false;
     }
 
     public static boolean taExists(int id, ArrayList<Person> people) {
         for (Person p : people) {
-            if (p.id == id && p instanceof TA) {
+            if (p.id == id && (p instanceof TA || p instanceof Student)) {
                 return true;
             }
         }
@@ -201,16 +340,25 @@ public class Project2 {
         return false;
     }
 
+    public static Student findStudent(int id, ArrayList<Person> people) {
+        Student s = new Student();
+        for (Person p : people) {
+            if (p.id == id && (p instanceof Student)) {
+                s = (Student) p;
+            }
+        }
+        return s;
+    }
+
     public static TA findTA(int id, ArrayList<Person> people) {
         TA t = new TA();
         for (Person p : people) {
-            if (p.id == id && p instanceof TA) {
-                t= (TA) p;
+            if (p.id == id && (p instanceof TA || p instanceof Student)) {
+                t = (TA) p;
             }
         }
         return t;
     }
-
 
     public static Faculty findFaculty(int id, ArrayList<Person> people) {
         Faculty f = new Faculty();
@@ -252,7 +400,7 @@ public class Project2 {
     }
 
     public static void displayGoodbye() {
-        System.out.println("Goodbye! ");
+        System.out.println("Goodbye!");
     }
 }
 
@@ -309,7 +457,6 @@ abstract class Person {
     public String name;
     public int id;
 
-
     public Person() {
     }
 
@@ -347,18 +494,20 @@ class Faculty extends Person {
 class Student extends Person {
     public boolean graduate;
     public ArrayList<Lecture> lecturesTaken;
-
+    public ArrayList<Lab> labsTaken;
 
     public Student() {
     }
 
-    public Student(String name, int id, boolean graduate, ArrayList<Lecture> lecturesTaken) {
+    public Student(String name, int id, boolean graduate, ArrayList<Lecture> lecturesTaken, ArrayList<Lab> labsTaken) {
         super(name, id);
         this.graduate = graduate;
         this.lecturesTaken = lecturesTaken;
+        this.labsTaken = labsTaken;
     }
 
     public String toString() {
+        // TODO print lab too
         return super.toString() + "\nLectures Taken: " + lecturesTaken + "\n----------------------------";
     }
 
@@ -366,7 +515,7 @@ class Student extends Person {
 
 class TA extends Student {
     public ArrayList<Lab> labsSupervised;
-    public Faculty advisor;
+    public String advisorName;
     public String expectedDegree;
 
     public void addLabSupervised(Lab l) {
@@ -376,11 +525,19 @@ class TA extends Student {
     public TA() {
     }
 
-    public TA(String name, int id, boolean graduate, ArrayList<Lecture> lecturesTaken, ArrayList<Lab> labsSupervised,
-            Faculty advisor, String expectedDegree) {
-        super(name, id, graduate, lecturesTaken);
+    public TA(String name, int id, boolean graduate, String advisorName, String expectedDegree) {
+        super(name, id, graduate, new ArrayList<Lecture>(), new ArrayList<Lab>());
+        this.labsSupervised = new ArrayList<Lab>();
+        this.advisorName = advisorName;
+        this.expectedDegree = expectedDegree;
+    }
+
+    public TA(String name, int id, boolean graduate, ArrayList<Lecture> lecturesTaken, ArrayList<Lab> labsTaken,
+            ArrayList<Lab> labsSupervised,
+            String advisorName, String expectedDegree) {
+        super(name, id, graduate, lecturesTaken, labsTaken);
         this.labsSupervised = labsSupervised;
-        this.advisor = advisor;
+        this.advisorName = advisorName;
         this.expectedDegree = expectedDegree;
     }
 
